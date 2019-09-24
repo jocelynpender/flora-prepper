f6bf008354f88a035f66b14cb971d19e76978d64
Jocelyn Pender <jpend049@uottawa.ca>
2019-09-24 15:54:01 -0400

    initialize repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Prepare flora documents from OCR to XML for CharaParser.',
+    author='Jocelyn Pender',
+    license='BSD-3',
+)
