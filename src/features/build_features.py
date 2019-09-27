d6bccb5f265a2e642ade00376a5703ac0f46b2aa
Jocelyn Pender <jpend049@uottawa.ca>
2019-09-27 16:54:00 -0400

    build featuers for both fna and bc

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,44 +1,59 @@
 # also encode in the file the option to produce an tf/idf not just dtm
-
 # tokenizer to remove unwanted elements from out data like symbols and numbers
-
 #  https://www.datacamp.com/community/tutorials/text-analytics-beginners-nltk
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.model_selection import train_test_split
-from src.data.make_fna import *
-from src.features.build_fna import *
 
 
-def build_fna_dtm():
-    fna = make_fna_data_frame(fna_filepath="data/external/fna_keys.csv", balance_categories=True)
-    tokenized_stop_words = prepare_stop_words(fna_stop_words = ["unknown", "accepted", "synonym", "basionym"])
+def flora_tokenizer(input_string):
+    """Apply custom tokenization to fna strings"""
+    assert type(input_string) == str, 'Text column not string format'
+    input_string = word_tokenize(input_string)
+    assert type(input_string) == list, 'Tokens not returned in text column as list'
+    return input_string
 
-    custom_vec = CountVectorizer(lowercase=True, tokenizer=fna_tokenize, stop_words=tokenized_stop_words, ngram_range=(1, 1))
-    text_counts = custom_vec.fit_transform(fna['text'])  # Build Document-Term-Matrix
 
+def build_nltk_stop_words(stop_words):
+    """Takes a set of stop words and add nltk's english words to it"""
+    import nltk
+    nltk.download('stopwords')
+    nltk_stop_words = set(stopwords.words('english'))
+    assert len(nltk_stop_words) > 0, 'NLTK words not loaded'
+    stop_words += nltk_stop_words
+    return stop_words
+
+
+def prepare_stop_words(fna_stop_words, include_nltk_stop_words=True):
+    """Tokenize the stop words"""
+    assert type(fna_stop_words) == list, 'Stop words not provided as a list'
+    if include_nltk_stop_words:
+        fna_stop_words = build_nltk_stop_words(fna_stop_words)
+    tokenized_stop_words = []
+    for word in fna_stop_words:
+        tokenized_stop_words.extend(word_tokenize(word))
+    tokenized_stop_words = frozenset(tokenized_stop_words)
+    return tokenized_stop_words
+
+
+def build_dtm_text_counts(flora_tokenizer, tokenized_stop_words, data_frame):
+    custom_vec = CountVectorizer(lowercase=True, tokenizer=flora_tokenizer, stop_words=tokenized_stop_words,
+                                 ngram_range=(1, 1))
+    text_counts = custom_vec.fit_transform(data_frame['text'])  # Build Document-Term-Matrix
+    return text_counts
+
+
+def build_tfidf_text_counts(flora_tokenizer, tokenized_stop_words, data_frame):
+    custom_vec = TfidfVectorizer(lowercase=True, tokenizer=flora_tokenizer, stop_words=tokenized_stop_words,
+                                 ngram_range=(1, 1))
+    text_counts = custom_vec.fit_transform(data_frame['text'])  # Build Document-Term-Matrix
+    return text_counts
+
+
+def build_train_test_split(text_counts, data_frame):
     X_train, X_test, y_train, y_test = train_test_split(
-        text_counts, fna['classification'], test_size=0.3, random_state=1)
-
-    return text_counts, X_train, X_test, y_train, y_test
-
-
-
-
-def build_fna_tfidf():
-    fna = make_fna_data_frame(fna_filepath="data/external/fna_keys.csv", balance_categories=True)
-    tokenized_stop_words = prepare_stop_words(fna_stop_words = ["unknown", "accepted", "synonym", "basionym"])
-
-    custom_vec = TfidfVectorizer(lowercase=True, tokenizer=fna_tokenize, stop_words=tokenized_stop_words, ngram_range=(1, 1))
-    text_counts = custom_vec.fit_transform(fna['text'])  # Build Document-Term-Matrix
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        text_counts, fna['classification'], test_size=0.3, random_state=1)
-
-    return text_counts, X_train, X_test, y_train, y_test
-
-text_counts, X_train, X_test, y_train, y_test = build_fna_tfidf()
-
+        text_counts, data_frame['classification'], test_size=0.3, random_state=1)
+    return X_train, X_test, y_train, y_test
 
 # Restrict features to frequent terms
 
