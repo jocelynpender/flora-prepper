95011e2f75a3455848c9d9eaebfbb50de2899892
Jocelyn Pender <jpend049@uottawa.ca>
2019-10-08 14:29:42 -0400

    update defaults and merge fna habitat category with distribution

--- src/data/make_fna.py
+++ src/data/make_fna.py
@@ -15,11 +15,15 @@ def read_fna_csv(fna_filepath):
     return fna
 
 
-def make_fna_data_frame(fna_filepath="data/external/fna_keys.csv",
+def make_fna_data_frame(fna_filepath="../data/external/fna_with_habitat.csv",
                         frac_to_sample=0.1, balance_categories=False,
-                        categories_to_keep=["key", "morphology", "taxon_identification"]):
+                        categories_to_keep=["key", "morphology", "taxon_identification", "distribution", "habitat"],
+                        rename_habitat=True):
     """Run all Flora of North America (FNA) processing tasks"""
     fna = read_fna_csv(fna_filepath)
     trimmed_fna = trim_categories(fna, categories_to_keep)
+    if rename_habitat:
+        trimmed_fna.loc[trimmed_fna.classification == "habitat", "classification"] = "distribution"
+        trimmed_fna.classification.cat.remove_unused_categories(inplace=True)
     sampled_fna = sample_flora(trimmed_fna, frac_to_sample, balance_categories)
     return sampled_fna
