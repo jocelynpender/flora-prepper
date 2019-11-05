f9a20c4efd4b05b6ae280cc8b73a6cfaebd7b420
Jocelyn Pender <jpend049@uottawa.ca>
2019-11-05 15:27:28 -0500

    pull wikipedia straight from api

--- notebooks/wikipedia_run.py
+++ notebooks/wikipedia_run.py
@@ -13,6 +13,7 @@ import os
 import sys
 import numpy as np
 import pandas as pd
+import wikipedia
 
 # Import custom modelling code
 module_path = os.path.abspath(os.path.join('../'))
@@ -22,6 +23,8 @@ if module_path not in sys.path:
 from src.models.run_model import *
 from src.visualization.visualize import *
 import src.features as features
+
+# pip install git+https://github.com/lucasdnd/Wikipedia.git --upgrade
 
 
 # In[2]:
@@ -35,14 +38,25 @@ train_indices = list(range(0, flora_data_frame.shape[0]))
 # In[3]:
 
 
+page = wikipedia.page("Cirsium arvense")
+page_sections = page.sections
+parsed_page = [(page_section_name, page.section(page_section_name)) for page_section_name in page_sections]
+wiki_data = pd.DataFrame(parsed_page, columns = ['classification', 'text'])
+wiki_data
+
+
+# In[4]:
+
+
 # Import of Wikipedia dataset
-wiki = pd.read_csv("../data/raw/cirsium_arvense_wikipedia.csv", index_col=None)
+# wiki = pd.read_csv("../data/raw/cirsium_arvense_wikipedia.csv", index_col=None)
+wiki = wiki_data
 test_indices = list(range(flora_data_frame.shape[0] + 1, flora_data_frame.shape[0] + wiki.shape[0]))
 flora_data_frame = pd.concat([flora_data_frame, wiki], ignore_index=True)
 flora_data_frame
 
 
-# In[4]:
+# In[5]:
 
 
 # Customize stop words for model
@@ -52,11 +66,9 @@ tokenized_stop_words = features.prepare_stop_words(custom_stop_words=["unknown",
 # Build DTM
 custom_vec, dtm_text_counts = build_dtm_text_counts(features.flora_tokenizer, tokenized_stop_words, flora_data_frame)
 dtm_text_counts.toarray()
-print(dtm_text_counts.shape)
-print(flora_data_frame.shape)
 
 
-# In[5]:
+# In[6]:
 
 
 # Prepare data for the model
@@ -66,7 +78,7 @@ X_test = dtm_text_counts[test_indices]
 y_test = flora_data_frame.iloc[test_indices].classification
 
 
-# In[6]:
+# In[7]:
 
 
 clf = MultinomialNB().fit(X_train, y_train)
