27f81db19820b4780867a9045a6d054b61a6b451
Jocelyn Pender <jpend049@uottawa.ca>
2019-11-12 14:10:27 -0500

    add ability to reclassify keys or habitat

--- src/run_features.py
+++ src/run_features.py
@@ -6,13 +6,13 @@ import pandas as pd
 from features import build_dtm_text_counts, prepare_stop_words, flora_tokenizer
 
 
-def main(flora_data_frame_path, dump_path, custom_stop_words_path):
+def main(flora_data_frame_path, dump_path, custom_stop_words_path, reclassify_keys, reclassify_habitat):
     """This is my default feature build with no cleaning regime and no length features.
 
     :param flora_data_frame_path: The training dataset to build features and the model from
     :param dump_path: Where to save the DTM vectorizer and the classifier model
     :param custom_stop_words_path: Where to find custom stop words to be used in the model
-    :return:
+    :return: joblib dump of the DTM and the vectorizer objects
     """
 
     logger = logging.getLogger(__name__)
@@ -20,6 +20,10 @@ def main(flora_data_frame_path, dump_path, custom_stop_words_path):
 
     # Import the training dataset
     flora_data_frame = pd.read_csv(flora_data_frame_path, index_col=0)
+    if reclassify_keys == 'yes':
+        flora_data_frame.classification[flora_data_frame.classification == "key"] = "morphology"
+    if reclassify_habitat == 'yes':
+        flora_data_frame.classification[flora_data_frame.classification == "habitat"] = "morphology"
 
     # Import custom stop words and build tokenized stopwords objects
     custom_stop_words_list = open(custom_stop_words_path).read().splitlines()
@@ -44,6 +48,11 @@ if __name__ == '__main__':
     parser.add_argument('--features_save_path', type=str, default="models/", help='path to save features')
     parser.add_argument('--custom_stop_words_path', type=str, default="models/stop_words.txt",
                         help='a list of stop words')
+    parser.add_argument('--reclassify_keys', type=str, default="no", help='should content be classified as '
+                                                                                 'keys (yes/no)')
+    parser.add_argument('--reclassify_habitat', type=str, default="no", help='should content be classified '
+                                                                                    'as habitat (yes/no)')
     args = parser.parse_args()
 
-    main(args.train_file_path, args.features_save_path, args.custom_stop_words_path)
+    main(args.train_file_path, args.features_save_path, args.custom_stop_words_path, args.reclassify_keys,
+         args.reclassify_habitat)
